827ee22aad7f9ddd83badcd7044c411c532f55f0
Daniel-Xu <xwd.forever@gmail.com>
2014-05-11 16:17:14 -0400

    add 50 largest population city

--- agricultural_choropleth/color.js
+++ agricultural_choropleth/color.js
@@ -19,10 +19,11 @@ var projection = d3.geo.albersUsa()
 var path = d3.geo.path()
                 .projection(projection)
 
-
 //color
 //http://colorbrewer2.org/
 var color = d3.scale.quantize().range(['rgb(239,243,255)','rgb(189,215,231)','rgb(107,174,214)','rgb(49,130,189)','rgb(8,81,156)'])
+
+var popuScale = d3.scale.sqrt().range([3, 35])
 
 //state value
 d3.csv("agricultural_choropleth/us-ag-productivity-2004.csv", function(csvData){
@@ -31,7 +32,7 @@ d3.csv("agricultural_choropleth/us-ag-productivity-2004.csv", function(csvData){
         d3.max(csvData, function(d){return d.value})
     ])
     
-    d3.json('../us-states.json', function(mapData){
+    d3.json('us-states.json', function(mapData){
         //we will change map the mapData to a mixed array with geodata and colorData 
         mapData.features = _.map(mapData.features, function(geoFeatures){
             var csvObj = _.find(csvData, function(csvD){
@@ -56,8 +57,32 @@ d3.csv("agricultural_choropleth/us-ag-productivity-2004.csv", function(csvData){
               else 
                   return "#ccc"
           })
+
+          //only the largest 50 cities in population
+          d3.csv("agricultural_choropleth/us-cities.csv", function(citiesObj){
+              svg.selectAll("circle")
+              .data(citiesObj)
+              .enter()
+              .append("circle")
+              .attr({
+                  cx: function(d){return projection([d.lon, d.lat])[0] },
+                  cy: function(d){return projection([d.lon, d.lat])[1] },
+                  r:  function(d) {
+                      popuScale.domain([
+                          d3.min(citiesObj, function(data){return parseInt(data.population)}),
+                          d3.max(citiesObj, function(data){return parseInt(data.population)})
+                      ])
+                  
+                      //return Math.sqrt(parseInt(d.population) * 0.00004);
+                      return popuScale(d.population)
+                  }             
+              })
+              .style({
+                  fill: "yellow", 
+                  opacity: 0.75
+              })
+          })
     })
+
 })
            
-
-
